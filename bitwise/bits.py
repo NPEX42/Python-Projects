0e100b63a64e73db79b0ea23f95ad9931b7adb20
Npex <gvenn666@gmail.com>
2020-09-09 14:21:51 +0100

    Update bits.py

--- bitwise/bits.py
+++ bitwise/bits.py
@@ -1,3 +1,34 @@
 #! /bin/env python3
+
 def NOT(val):
     return ~val
+
+def OR(a,b):
+    return (a | b)
+
+def EXOR(a,b):
+    return (a ^ b)
+
+def AND(a,b):
+    return (a & b)
+
+def BIT(a, idx):
+    mask = 1 << idx
+    return bool(a & mask
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                )
